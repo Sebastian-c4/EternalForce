cb0cd5405f7b1d03ef35e82f91e7e41df1b120d2
c4_angel <20829444@qq.com>
2018-08-25 10:32:47 +0800

    Version 4

---
+++ c4EternalForce/dialog/c4dialog.d
@@ -0,0 +1,14 @@
+
+BEGIN c4EFDia
+
+IF ~GlobalGT("c4EF_Weapon_Proficiency","Locals",0)~ THEN BEGIN learned
+	SAY @0
+	IF ~~ THEN EXIT
+END
+
+IF ~Global("c4EF_Weapon_Proficiency","Locals",0)~ THEN BEGIN choose_prof
+	SAY @1
+	++ @2 DO	~ReallyForceSpellRES("c4efPF10",Myself)~ FLAGS ~%BIT9%~ EXIT
+	++ @3 DO	~ReallyForceSpellRES("c4efPF20",Myself)~ FLAGS ~%BIT9%~ EXIT
+	++ @4 DO	~ReallyForceSpellRES("c4efPF30",Myself)~ FLAGS ~%BIT9%~ EXIT
+END
