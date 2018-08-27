62ca5924832abdb373db455fb29bc563345da3e1
c4_angel <20829444@qq.com>
2018-08-28 00:16:33 +0800

    form6 fix + auto form1

--- c4EternalForce/dialog/c4dialog.d
+++ c4EternalForce/dialog/c4dialog.d
@@ -8,7 +8,7 @@ END
 
 IF ~Global("c4EF_Weapon_Proficiency","Locals",0)~ THEN BEGIN choose_prof
 	SAY @1
-	++ @2 DO	~ReallyForceSpellRES("c4efPF10",Myself)~ FLAGS ~%BIT9%~ EXIT
-	++ @3 DO	~ReallyForceSpellRES("c4efPF20",Myself)~ FLAGS ~%BIT9%~ EXIT
-	++ @4 DO	~ReallyForceSpellRES("c4efPF30",Myself)~ FLAGS ~%BIT9%~ EXIT
+	++ @2 DO	~ReallyForceSpellRES("c4efPF10",Myself) ReallyForceSpellRES("c4efJj1-",Myself)~ FLAGS ~%BIT9%~ EXIT
+	++ @3 DO	~ReallyForceSpellRES("c4efPF20",Myself) ReallyForceSpellRES("c4efJj1-",Myself)~ FLAGS ~%BIT9%~ EXIT
+	++ @4 DO	~ReallyForceSpellRES("c4efPF30",Myself) ReallyForceSpellRES("c4efJj1-",Myself)~ FLAGS ~%BIT9%~ EXIT
 END
